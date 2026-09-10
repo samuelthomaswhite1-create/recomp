2f70723416e1fad031057f1595018e7e56ba245e
samuelthomaswhite1-create <samuelthomaswhite1@gmail.com>
2026-09-10 12:44:59 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Recomp Tracker service worker
 // Bump CACHE_VERSION when you upload a new index.html so phones pick up the change.
-const CACHE_VERSION = "recomp-v6";
+const CACHE_VERSION = "recomp-v7";
 const ASSETS = ["./", "./index.html"];
 
 self.addEventListener("install", event => {
